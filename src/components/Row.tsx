dd083f6fb697dea0b0d37bca4a0a7cb3d2892a41
Victor Tisbierek <metropoller@gmail.com>
2023-04-18 13:45:25 -0300

    fixing row modal button

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -19,8 +19,8 @@ export default function Row({pictures, modal}: RowProps){
     return (
         <div className={styles.container}>
             {pictures.map(picture => (
-                <div className={styles.pictureBox}>
-                    <button onClick={() => modal(true)}>
+                <button onClick={() => modal(true)} className={styles.modalButton}>
+                    <div className={styles.pictureBox}>
                         <Image src={picture.url} alt={picture.alt}/>
                         <div className={styles.pictureText}>
                             <div className={styles.pictureCaption}>
@@ -29,8 +29,8 @@ export default function Row({pictures, modal}: RowProps){
                             </div>
                             <p className={styles.tags}>{picture.tags.map(tag => `${tag} `)}</p>
                         </div>
-                    </button>
-                </div>
+                    </div>
+                </button>
             ))}
         </div>
     );
