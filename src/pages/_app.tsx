3748dfe8c655f0e0f6059fab8dc9bff8962c23f3
Victor Tisbierek <metropoller@gmail.com>
2023-05-05 12:08:20 -0300

    wrapped the app in the channel plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app'
+import {ReactChannelIO} from 'react-channel-plugin';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ReactChannelIO pluginKey={process.env.NEXT_PUBLIC_CHANNELIO_PLUGIN_KEY as string} language="en" autoBoot>
+      <Component {...pageProps} />
+    </ReactChannelIO>
+  )
 }
