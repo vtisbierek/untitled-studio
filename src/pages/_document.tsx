55044c959ae0099369484c57b2e6e9f8062db169
Victor Tisbierek <metropoller@gmail.com>
2023-05-17 17:32:11 -0300

    adding temporary favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:wght@400;500;700&family=Noto+Sans+KR:wght@400;500;700;900&family=Noto+Sans:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:wght@400;500;700&family=Noto+Sans+KR:wght@400;500;700;900&family=Noto+Sans:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
       </Head>
       <body>
         <Main />
